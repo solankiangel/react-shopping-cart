8918c1f75d5d888028b80ad90da7ecb1592c32c1
solankiangel <antara.solanki2811@gmail.com>
2019-02-16 12:10:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import {Switch,Route} from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+
+class App extends Component {
+  render() {
+    return (
+  <React.Fragment>
+    <NavBar/>
+    <Switch>
+      <Route exact path = "/" component={ProductList}/>
+      <Route  path = "/details" component={Details}/>
+      <Route  path = "/cart" component={Cart}/>
+      <Route component={Default}/>
+    </Switch>
+    <Modal />
+  </React.Fragment>
+
+
+    );
+  }
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,113 @@
+export const storeProducts =[
+    {
+        id:1,
+        title: "GOOGLE PIXEL-BLACK",
+        img:"img/product-1.png ",
+        price:10,
+        company: "GOOGLE",
+        info:"Google announced the first generation Pixel smartphones, the Pixel and the Pixel XL, on October 4, 2016 during the #MadeByGoogle event.Google emphasized the camera on the two phones, which ranked as the best smartphone camera on DxOMarkMobile with 90 points until HTC released the U11, which also scored 90 points. This is largely due to software optimizations such as HDR+. The Pixel phones also include unlimited cloud storage for pictures on Google Photos and, for devices purchased directly from Google, an unlockable bootloader.",
+        inCart: false,
+        count:0,
+        total:0
+    },
+    {
+        id:2,
+        title: "SAMSUNG S7",
+        img:"img/product-2.png ",
+        price:16,
+        company: "Samsung",
+        info:"The Galaxy S7's hardware design is largely identical to that of the S6; Unlike the S6 though, Samsung has decided to remove the built-in IR Blaster due to low demand. The device retains its metal and glass chassis, but with refinements such as a square home button, and a thinner protrusion for the camera. Both models are available in black and gold colors; white, pink, blue and silver versions are available depending on market.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:3,
+        title: "HTC 10-White",
+        img:"img/product-3.png ",
+        price:18,
+        company: "HTC",
+        info:"The HTC 10 features an aluminum body with a unibody design. The dual front-facing audio speakers from previous HTC smartphones (such as the HTC One M9) have been removed. The phone still has 2 speakers; however, one is on the top bezel, while the other is on the bottom edge of the phone. The HTC 10 also includes a fingerprint sensor under the 5.2-inch 1440x2560 Gorilla Glass 3 screen.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:4,
+        title: "HTC Desire 626s",
+        img:"img/product-4.png ",
+        price:24,
+        company: "HTC",
+        info:"The HTC Desire 626s is an Android-powered smartphone manufactured by HTC. It is available on Cricket (a subsidiary of AT&T), T-Mobile, MetroPCS (a T-Mobile subsidiary), Sprint Prepaid, and Virgin Mobile (a Sprint subsidiary) in the United States. The phone was announced in July 2015.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:5,
+        title: "Vintage Iphone",
+        img:"img/product-5.png ",
+        price:17,
+        company: "apple",
+        info:" All generations of the iPhone use Apple's iOS mobile operating system software. The first-generation iPhone was released on June 29, 2007, and multiple new hardware iterations with new iOS releases have been released since.The user interface is built around the device's multi-touch screen, including a virtual keyboard. The iPhone has Wi-Fi and can connect to cellular networks",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:6,
+        title: "Iphone7",
+        img:"img/product-6.png ",
+        price:24,
+        company: "apple",
+        info:"Apple has released eleven generations of iPhone models, each accompanied by one of the eleven major releases of the iOS operating system.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:7,
+        title: "Oppo F9-Pro",
+        img:"img/product-7.png ",
+        price:2,
+        company: "Oppo",
+        info:"The OPPO F9 is powered by a 2GHz octa-core processor, and has 4/6GB of RAM and 64GB of storage.It operates on ColorOS 5.2 which is a customized version of Android 8.1 (Oreo). It has both a 25 MP front camera and dual 16 + 2 MP rear cameras. It has a 3500mAh battery and is powered by VOOC fast wired charging which delivers 2 hours of talk time with 5 minutes of charging.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+    {
+        id:8,
+        title: "Vivo v11-Pro",
+        img:"img/product-8.png ",
+        price:2,
+        company: "Vivo",
+        info:"Vivo, named after the Esperanto word for life is a Chinese technology company owned by BBK Electronics that makes smartphones, smartphone accessories, software, and online services. It was founded in 2009 in Dongguan, China. The company develops software for their phones such as the Vivo App Store, iManager included in their proprietary Android-based operating system called Funtouch OS.",
+        inCart: false,
+        count:0,
+        total:0
+
+    },
+
+];
+
+export const detailProduct={
+    
+        id:1,
+        title: "Google Pixel -Black",
+        img:"img/product-1.png ",
+        price:10,
+        company: "GOOGLE",
+        info:"Google announced the first generation Pixel smartphones, the Pixel and the Pixel XL, on October 4, 2016 during the #MadeByGoogle event.Google emphasized the camera on the two phones, which ranked as the best smartphone camera on DxOMarkMobile with 90 points until HTC released the U11, which also scored 90 points. This is largely due to software optimizations such as HDR+. The Pixel phones also include unlimited cloud storage for pictures on Google Photos and, for devices purchased directly from Google, an unlockable bootloader.",
+        inCart: false,
+        count:0,
+        total:0
+    
+
+};
